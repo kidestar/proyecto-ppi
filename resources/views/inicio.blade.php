99341824190d58455b291ba638e6ded8c0de6db3
unknown <sebastian_14253@hotmail.com>
2021-07-02 11:10:49 -0500

    inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.windmill')
 @section('contenido')
-<h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-    Inicio
+<h2 class="my-6 text-l font-semibold text-gray-700 dark:text-gray-200">
+    <img aria-hidden="true" class="object-cover w-full h-full dark:hidden"
+        src="{{ asset('img/login.jpg') }}" alt="Perro" />
 </h2>
 @endsection
